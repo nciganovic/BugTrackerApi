d43a7e1a8f3462affdabcd59f6a2c53e222d80bc
nciganovic <nciganovic99@gmail.com>
2021-04-26 21:29:08 +0200

    Create TicketPriority table

---
+++ BugTracker/Domain/TicketPriority.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class TicketPriority : IBaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
