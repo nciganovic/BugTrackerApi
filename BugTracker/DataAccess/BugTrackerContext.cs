55e979442c146ccd62ba258908929635ea0faffb
nciganovic <nciganovic99@gmail.com>
2021-04-26 21:29:39 +0200

    Change connection string

--- BugTracker/DataAccess/BugTrackerContext.cs
+++ BugTracker/DataAccess/BugTrackerContext.cs
@@ -4,19 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
 
 namespace DataAccess
 {
     public class BugTrackerContext : DbContext
     {
+        public DbSet<TicketPriority> TicketPriorities { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=BugTracker;Integrated Security=True");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BugTracker;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
         }
+
+        
     }
 }
