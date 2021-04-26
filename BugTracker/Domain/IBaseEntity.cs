f727607d9bdefbb11addd5b94535d6aec22e0534
nciganovic <nciganovic99@gmail.com>
2021-04-26 21:30:10 +0200

    Add IBaseEntity interface

---
+++ BugTracker/Domain/IBaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public interface IBaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
