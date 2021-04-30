14e2c27aba5f353ffb3738a94b29804e10c0a3fd
nciganovic <nciganovic99@gmail.com>
2021-04-30 11:21:20 +0200

    Add base columns to ProjectApplicaitonUSer

--- BugTracker/DataAccess/Migrations/20210430091710_Add_Base_Columns_To_ProjectApplicationUser_Table.Designer.cs
+++ BugTracker/DataAccess/Migrations/20210430091710_Add_Base_Columns_To_ProjectApplicationUser_Table.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(BugTrackerContext))]
-    partial class BugTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20210430091710_Add_Base_Columns_To_ProjectApplicationUser_Table")]
+    partial class Add_Base_Columns_To_ProjectApplicationUser_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
