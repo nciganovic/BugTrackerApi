ad9cd1f83c9320680ac239f1b5d050b61b48f2b8
nciganovic <nciganovic99@gmail.com>
2021-05-03 12:42:40 +0200

    Create IRoleCommand interface

---
+++ BugTracker/Application/Commands/IRoleCommands.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+    public interface IRoleCommands
+    {
+        public void Create(Role role);
+        public IEnumerable<Role> Read();
+        public Role Read(int id);
+        public void Update(Role role);
+        public Role Delete(int id);
+    }
+}
