315841d473538e865cdead5fae08d6b63c3faceb
nciganovic <nciganovic99@gmail.com>
2021-05-03 13:47:36 +0200

    CRUD for Roles

---
+++ BugTracker/Api/Controllers/RoleController.cs
@@ -0,0 +1,71 @@
+﻿using Application.Commands;
+using Application.Dto;
+using AutoMapper;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleCommands roleCommands;
+        private readonly IMapper mapper;
+
+        public RoleController(IRoleCommands roleCommands, IMapper mapper)
+        {
+            this.roleCommands = roleCommands;
+            this.mapper = mapper;
+        }
+
+        // GET: api/<RoleController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Role> role = roleCommands.Read();
+            return Ok(role);
+        }
+
+        // GET api/<RoleController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Role role = roleCommands.Read(id);
+            return Ok(role);
+        }
+
+        // POST api/<RoleController>
+        [HttpPost]
+        public IActionResult Post([FromBody] RoleDto roleDto)
+        {
+            Role role = mapper.Map<Role>(roleDto);
+            roleCommands.Create(role);
+            return Ok("Role created successfully");
+        }
+
+        // PUT api/<RoleController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] RoleDto roleDto)
+        {
+            roleDto.Id = id;
+            Role role = mapper.Map<Role>(roleDto);
+            roleCommands.Update(role);
+            return Ok("Role updated successfully");
+        }
+
+        // DELETE api/<RoleController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            roleCommands.Delete(id);
+            return Ok($"Role with id = {id} deleted successfully");
+        }
+    }
+}
---
+++ BugTracker/EfCommands/RoleCommands.cs
@@ -0,0 +1,96 @@
+﻿using Application.Commands;
+using Application.Exceptions;
+using DataAccess;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCommands
+{
+    public class RoleCommands : BaseCommands, IRoleCommands
+    {
+        public RoleCommands(BugTrackerContext bugTrackerContext) : base(bugTrackerContext)
+        {
+
+        }
+
+        public void Create(Role role)
+        {
+            if (IsNameAlreadyTaken(role.Name))
+            {
+                throw new EntityAlreadyExists();
+            }
+
+            context.Add(role);
+            context.SaveChanges();
+        }
+
+        public Role Delete(int id)
+        {
+            Role item = context.Roles.Find(id);
+
+            if (item == null)
+                throw new EntityNotFoundException();
+
+            item.DeletedAt = DateTime.Now;
+
+            context.Roles.Update(item);
+            context.SaveChanges();
+            return item;
+        }
+
+        public IEnumerable<Role> Read()
+        {
+            return context.Roles.Select(x => new Role
+            {
+                Id = x.Id,
+                Name = x.Name,
+                CreatedAt = x.CreatedAt,
+                UpdatedAt = x.UpdatedAt,
+                DeletedAt = x.DeletedAt
+            }).ToList();
+        }
+
+        public Role Read(int id)
+        {
+            return context.Roles.Where(x => x.Id == id).Select(x => new Role
+            {
+                Id = x.Id,
+                Name = x.Name,
+                CreatedAt = x.CreatedAt,
+                UpdatedAt = x.UpdatedAt,
+                DeletedAt = x.DeletedAt
+            }).ToList().FirstOrDefault();
+        }
+
+        public void Update(Role role)
+        {
+            Role item = context.Roles.Find(role.Id);
+
+            if (item == null)
+                throw new EntityNotFoundException();
+
+            context.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Detached;
+
+            if (IsNameAlreadyTaken(role.Name))
+                throw new EntityAlreadyExists();
+
+            var tp = context.Roles.Attach(role);
+            tp.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        private bool IsNameAlreadyTaken(string name)
+        {
+            if (context.Roles.Any(x => x.Name == name))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
