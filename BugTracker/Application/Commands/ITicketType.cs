3b0541d0a878dc69a81a6fa16975476f1356d664
nciganovic <nciganovic99@gmail.com>
2021-05-03 11:27:59 +0200

    Create TicketType interface

---
+++ BugTracker/Application/Commands/ITicketType.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+    public interface ITicketType
+    {
+        public void Create(TicketType ticketType);
+        public IEnumerable<TicketType> Read();
+        public TicketType Read(int id);
+        public void Update(TicketType ticketType);
+        public TicketType Delete(int id);
+    }
+}
