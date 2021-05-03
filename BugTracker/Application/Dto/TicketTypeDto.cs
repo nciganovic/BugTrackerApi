b51ed8e75ce2ed98fcffd6bbe41423e9cc1e6319
nciganovic <nciganovic99@gmail.com>
2021-05-03 11:44:55 +0200

    Rename class to TicketTypeDto

--- BugTracker/Application/Dto/TicketTypeDto.cs
+++ BugTracker/Application/Dto/TicketTypeDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Application.Dto
 {
-    public class TicketStatusDto
+    public class TicketTypeDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
