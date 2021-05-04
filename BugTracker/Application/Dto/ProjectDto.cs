8656f672e045f9a31ce5fbf228568404e6298700
nciganovic <nciganovic99@gmail.com>
2021-05-04 14:17:41 +0200

    Create Dto for Project

---
+++ BugTracker/Application/Dto/ProjectDto.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto
+{
+    public class ProjectDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CompanyId { get; set; }
+        
+        public Company Company { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+        
+        public List<ProjectApplicationUser> ProjectApplicationUsers { get; set; }
+    }
+}
