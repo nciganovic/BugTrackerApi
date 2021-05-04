158c780ab7b0bb05b895b07b0ad6354359448d1d
nciganovic <nciganovic99@gmail.com>
2021-05-04 14:38:40 +0200

    Set requirements for dto data

--- BugTracker/Application/Dto/TicketTypeDto.cs
+++ BugTracker/Application/Dto/TicketTypeDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace Application.Dto
     public class TicketTypeDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
