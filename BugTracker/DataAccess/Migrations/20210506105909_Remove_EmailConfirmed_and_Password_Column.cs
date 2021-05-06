95d2f86be89b26683cdebc06c4ad7479aca56a62
nciganovic <nciganovic99@gmail.com>
2021-05-06 13:30:38 +0200

    Remove EmailConfirmed and Password columns

---
+++ BugTracker/DataAccess/Migrations/20210506105909_Remove_EmailConfirmed_and_Password_Column.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Remove_EmailConfirmed_and_Password_Column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmed",
+                table: "ApplicaitonUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "ApplicaitonUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailConfirmed",
+                table: "ApplicaitonUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "ApplicaitonUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- BugTracker/Domain/ApplicationUser.cs
+++ BugTracker/Domain/ApplicationUser.cs
@@ -13,9 +13,6 @@ namespace Domain
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public bool EmailConfirmed { get; set; }
-        public string Password { get; set; }
-        
         public int RoleId { get; set; }
         public Role Role { get; set; }
         public List<CompanyApplicationUser> CompanyApplicaitonUsers { get; set; }
