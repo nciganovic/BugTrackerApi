cef3f83cfe232b944e30f303c7efe8f6126f5d3c
nciganovic <nciganovic99@gmail.com>
2021-05-06 14:07:08 +0200

    Set validation for ApplicationUserDto

--- BugTracker/Application/Dto/ApplicationUserDto.cs
+++ BugTracker/Application/Dto/ApplicationUserDto.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,23 @@ namespace Application.Dto
     public class ApplicationUserDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
         public string LastName { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int RoleId { get; set; }
         public Role Role { get; set; }
+        
         public List<CompanyApplicationUser> CompanyApplicaitonUsers { get; set; }
         public List<ProjectApplicationUser> ProjectApplicaitonUsers { get; set; }
         public List<Ticket> IssuerTickets { get; set; }
