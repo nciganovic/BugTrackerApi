34e1d7f0579ac7fed9c394b4a6756329570034d0
nciganovic <nciganovic99@gmail.com>
2021-05-12 13:54:59 +0200

    Create ICommands interface

---
+++ BugTracker/Application/Interfaces/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICommand<TRequest>
+    {
+        void Excecute(TRequest request);
+    }
+
+    public interface ICommand<TRequest, TResult> {
+        TRequest Execute(TRequest request);
+    }
+}
