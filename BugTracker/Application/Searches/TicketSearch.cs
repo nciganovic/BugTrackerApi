c56c69f3d02e4cca8f9fcb9b564b9397322a7521
nciganovic <nciganovic99@gmail.com>
2021-05-18 11:10:47 +0200

    Create search for ticket

---
+++ BugTracker/Application/Searches/TicketSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Searches
+{
+    public class TicketSearch
+    {
+        public string Title { get; set; }
+        public int? Priority { get; set; }
+        public int? Status { get; set; }
+        public int? Type { get; set; }
+        public int? Issuer { get; set; }
+        public int? Developer { get; set; }
+        public bool? OnlyActive { get; set; }
+    }
+}
