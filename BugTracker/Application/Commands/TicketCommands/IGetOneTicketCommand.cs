0f45e9a4337b64c252d61f9cbb0089799b1af55e
nciganovic <nciganovic99@gmail.com>
2021-05-18 14:30:07 +0200

    Get one ticket commadn

---
+++ BugTracker/Application/Commands/TicketCommands/IGetOneTicketCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dto;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.TicketCommands
+{
+    public interface IGetOneTicketCommand : ICommand<int, TicketDto>
+    {
+    }
+}
