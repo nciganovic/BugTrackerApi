6fbd7494b351ad4a438226c27c9d57b82b11b961
nciganovic <nciganovic99@gmail.com>
2021-05-18 14:29:04 +0200

    Add requirements for fields and fix typo in DeveloperId

--- BugTracker/Application/Dto/TicketDto.cs
+++ BugTracker/Application/Dto/TicketDto.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,19 +16,26 @@ namespace Application.Dto
         public int? OriginalTicketId { get; set; }
         public Ticket OriginalTicket { get; set; }
 
+        [Range(0, 3)]
         public TicketPriority Priority { get; set; }
+
+        [Range(0, 4)]
         public TicketStatus Status { get; set; }
+
+        [Range(0, 1)]
         public TicketType Type { get; set; }
 
 
         public int IssuerId { get; set; }
         public ApplicationUser Issuer { get; set; }
 
-        public int DevloperId { get; set; }
+        public int DeveloperId { get; set; }
         public ApplicationUser Developer { get; set; }
 
+        [MaxLength(30)]
         public string Title { get; set; }
 
+        [MaxLength(150)]
         public string Description { get; set; }
 
         public List<Comment> Comments { get; set; }
