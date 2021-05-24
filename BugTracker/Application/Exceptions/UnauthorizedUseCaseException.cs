e054585059afef03287be06c17e570aa960b59e4
nciganovic <nciganovic99@gmail.com>
2021-05-24 16:56:19 +0200

    add required interfaces

---
+++ BugTracker/Application/Exceptions/UnauthorizedUseCaseException.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class UnauthorizedUseCaseException : Exception
+    {
+        public UnauthorizedUseCaseException(IUseCase useCase, IApplicationActor actor)
+            : base($"Actor with an id of {actor.Id} - {actor.Identity} tried to execute {useCase.Name}.")
+        {
+
+        }
+    }
+}
