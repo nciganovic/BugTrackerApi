d16b5cd5edd2a1bc33ed9da128417058b2216db0
nciganovic <nciganovic99@gmail.com>
2021-06-01 14:38:11 +0200

    Add AddCompanyDto and map it with Company

--- BugTracker/Application/MapperProfiles/DefaultProfile.cs
+++ BugTracker/Application/MapperProfiles/DefaultProfile.cs
@@ -20,6 +20,8 @@ namespace Application.MapperProfiles
             CreateMap<ChangeApplicationUserDto, ApplicationUser>();
 
             CreateMap<Company, CompanyDto>().ReverseMap();
+            CreateMap<AddCompanyDto, Company>();
+
             CreateMap<Project, ProjectDto>().ReverseMap();
             CreateMap<Ticket, TicketDto>().ReverseMap();
 
