e00c577fc40e55c4992e2d03fd8502f4587838a3
nciganovic <nciganovic99@gmail.com>
2021-06-04 09:40:57 +0200

    Put FakeApiActor and AdminFakeApiActor in separate files

---
+++ BugTracker/Api/Core/AdminFakeApiActor.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Core
+{
+    public class AdminFakeApiActor : IApplicationActor
+    {
+        public int Id => 2;
+
+        public string Identity => "Fake Admin";
+
+        public IEnumerable<int> AllowedUseCases => Enumerable.Range(1, 1000);
+    }
+}
--- BugTracker/Api/Core/FakeApiActor.cs
+++ BugTracker/Api/Core/FakeApiActor.cs
@@ -14,13 +14,4 @@ namespace Api.Core
 
         public IEnumerable<int> AllowedUseCases => new List<int> { 1 };
     }
-
-    public class AdminFakeApiActor : IApplicationActor
-    {
-        public int Id => 2;
-
-        public string Identity => "Fake Admin";
-
-        public IEnumerable<int> AllowedUseCases => Enumerable.Range(1, 1000);
-    }
 }
