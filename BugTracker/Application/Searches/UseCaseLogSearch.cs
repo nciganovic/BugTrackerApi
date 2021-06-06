1705134f506715625392dbeaa916a3fd2c72c934
nciganovic <nciganovic99@gmail.com>
2021-06-06 18:10:51 +0200

    Create Search class for UseCaseLog

---
+++ BugTracker/Application/Searches/UseCaseLogSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Searches
+{
+    public class UseCaseLogSearch
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string UseCaseNameKeyword { get; set; }
+        public string DataKeyword { get; set; }
+        public string ActorKeyword { get; set; }
+    }
+}
