23406a88c768b0683533be52971981b5e05c7dde
nciganovic <nciganovic99@gmail.com>
2021-06-06 14:04:50 +0200

    Add Dto to accept parameters for DELETE

---
+++ BugTracker/Application/Dto/RemoveApplicationUserCaseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto
+{
+    public class RemoveApplicationUserCaseDto
+    {
+        public int ApplicationUserId { get; set; }
+        public int UseCaseId { get; set; }
+    }
+}
