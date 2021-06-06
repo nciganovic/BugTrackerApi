5b928f973e42fc2ff0a0e4fefb7134b012b75d2b
nciganovic <nciganovic99@gmail.com>
2021-06-06 14:04:06 +0200

    Remove ApplicationUser Case command

---
+++ BugTracker/Application/Commands/ApplicationUserCaseCommands/IRemoveApplicationUserCaseCommand.cs
@@ -0,0 +1,16 @@
+﻿using Application.Dto;
+using Application.Interfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.ApplicationUserCaseCommands
+{
+    public interface IRemoveApplicationUserCaseCommand : ICommand<ApplicationUserCase>
+    {
+
+    }
+}
---
+++ BugTracker/Implementation/EfCommands/EfApplicationUserCaseCommands/EfRemoveApplicationUserCaseCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Commands.ApplicationUserCaseCommands;
+using DataAccess;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.EfCommands.EfApplicationUserCaseCommands
+{
+    public class EfRemoveApplicationUserCaseCommand : IRemoveApplicationUserCaseCommand
+    {
+        private readonly BugTrackerContext _context;
+
+        public EfRemoveApplicationUserCaseCommand(BugTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public int Id => 39;
+
+        public string Name => "Remove applicationUser case command";
+
+        public void Execute(ApplicationUserCase request)
+        {
+            _context.Remove(request);
+            _context.SaveChanges();
+        }
+    }
+}
