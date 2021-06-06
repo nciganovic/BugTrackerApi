491af6eda7124ada6f8b47f33232a6d629fbe359
nciganovic <nciganovic99@gmail.com>
2021-06-06 14:03:44 +0200

    Get one ApplicationUser case

---
+++ BugTracker/Implementation/Queries/ApplicationUserCaseQueries/EfGetOneApplicationUserCaseQuery.cs
@@ -0,0 +1,35 @@
+﻿using Application.Dto;
+using Application.Queries.ApplicationUserCaseQueries;
+using DataAccess;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Queries.ApplicationUserCaseQueries
+{
+    public class EfGetOneApplicationUserCaseQuery : IGetOneApplicationUserCaseQuery
+    {
+        private readonly BugTrackerContext _context;
+
+        public EfGetOneApplicationUserCaseQuery(BugTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public int Id => 40;
+
+        public string Name => "Get one applicationUser case query";
+
+        public ApplicationUserCase Execute(RemoveApplicationUserCaseDto search)
+        {
+            var item = _context.ApplicationUserCases
+                .FirstOrDefault(x => x.ApplicationUserId == search.ApplicationUserId
+                && x.UseCaseId == search.UseCaseId);
+
+            return item;
+        }
+    }
+}
