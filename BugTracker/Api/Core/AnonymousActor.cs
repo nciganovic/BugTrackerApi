4af4dde7a5e042c0f58828160a5bfa33f3fb7eb8
nciganovic <nciganovic99@gmail.com>
2021-06-06 17:18:10 +0200

    Add anonymous actor

---
+++ BugTracker/Api/Core/AnonymousActor.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Core
+{
+    public class AnonymousActor : IApplicationActor
+    {
+        public int Id => 3;
+
+        public string Identity => "Anonymous actor";
+
+        public IEnumerable<int> AllowedUseCases => new List<int> { 6 };
+    }
+}
